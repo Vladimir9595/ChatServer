b19e1541c2e20881fb075f88d3e7bf08224d04be
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-04-24 11:17:46 +0200

    Ajoutez des fichiers projet.

---
+++ ProjForms/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(308, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Click Here";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 20);
+            label1.TabIndex = 1;
+            label1.Text = "FORMULAIRE D'INFO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 2;
+            label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 20);
+            label4.TabIndex = 4;
+            label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 174);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Date of Birth";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 212);
+            label6.Name = "label6";
+            label6.Size = new Size(133, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Téléphone number";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1128, 527);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Creating and setting the label
+            Label mylab = new Label();
+            mylab.Text = "Inserer un Titre";
+            mylab.Location = new Point(222, 90);
+            mylab.AutoSize = true;
+            mylab.Font = new Font("Calibri", 18);
+            mylab.BorderStyle = BorderStyle.Fixed3D;
+            mylab.ForeColor = Color.Green;
+            mylab.Padding = new Padding(6);
+
+            // Adding this control to the form
+            this.Controls.Add(mylab);
+
+            // Creating and setting the label
+            Label mylab1 = new Label();
+            mylab1.Text = "Description";
+            mylab1.Location = new Point(155, 170);
+            mylab1.AutoSize = true;
+            mylab1.BorderStyle = BorderStyle.Fixed3D;
+            mylab1.Font = new Font("Calibri", 18);
+            mylab1.Padding = new Padding(6);
+
+            // Adding this control to the form
+            this.Controls.Add(mylab1);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
